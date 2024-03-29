f2066ac2e78d2a54fb09169b4cd6ff7a1bfb0c98
Cecilezeba <cecilezeba75@gmail.com>
2024-03-29 15:56:32 +0100

    creation de ma classe chien

---
+++ src/animaux/Chien.java
@@ -0,0 +1,39 @@
+package animaux;
+
+public class Chien extends Animal {
+	private String nom;
+	private int age;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Chien(String nom, int age) {
+		super();
+		this.nom = nom;
+		this.age = age;
+	}
+
+	public Chien() {
+		super();
+	}
+	
+	public void displayName() {
+		System.out.println("nom");
+	}
+	public void displayAge() {
+		System.out.println("age");
+	}
+}
