f661bf106ab1001dbfb2ae5555e4288858147f08
ArnaudFonzam <gui.fofou@umi2growcameroun.com>
2024-03-29 08:54:05 +0100

    setup the project

---
+++ src/animaux/Animal.java
@@ -0,0 +1,13 @@
+package animaux;
+
+
+public class Animal {
+	
+	public void displayName() {
+		System.out.println("This is My name");
+	}
+	public void displayAge() {
+		System.out.println("This is My age");
+	}
+	
+}
